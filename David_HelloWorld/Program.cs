ffa1365bbb0c6c659f95141ff6e2e367c4e41675
Boning Liu <liuboning2012@gmail.com>
2021-08-24 09:05:49 -0400

    calculate doubles and print them

--- David_HelloWorld/Program.cs
+++ David_HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace David_HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("David Liu");
+            Console.WriteLine("David Liu\n");
+            double a = 1, b = 2.5, c;
+            c = a / b;
+            Console.WriteLine($"{a}/{b}={c}");
         }
     }
 }
