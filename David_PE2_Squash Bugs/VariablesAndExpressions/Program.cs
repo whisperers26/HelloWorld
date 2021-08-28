4e65df98dbdc550fd9df89b8bf35fbf3ba0c173f
Liu Boning <liuboning2012@gmail.com>
2021-08-28 16:55:29 -0400

    answer of question 1

---
+++ David_PE2_Squash Bugs/VariablesAndExpressions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+namespace fabulous
+{
+    // code in fabulous namespace
+    using super;
+    class ReferToNameSpace
+    {
+        int a = super.smashing.GreatName.great;
+
+    }
+}
+
+namespace super
+{
+    namespace smashing
+    {
+        // great name defined
+        class GreatName
+        {
+            public static int great;
+        }
+    }
+}
+
