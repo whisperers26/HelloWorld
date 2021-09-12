797e3d678aa52996d2ade0b9bf7280ae525ea5bb
Liu Boning <liuboning2012@gmail.com>
2021-09-12 17:07:12 -0400

    complete q8

---
+++ David_PE8_Variables/PE8_q8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PE8_q8
+{
+    //summary
+    //this program let user input a string
+    //and replace all no with yes
+    //summary
+    class Program
+    {
+        //summary
+        //main method
+        //summary
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string: ");
+            string input = Console.ReadLine();
+            string output = "";
+            output = ReplaceCaseInsensitive(input, "no", "yes");
+            Console.WriteLine(output);
+        }
+
+        //summary
+        //replace old pattern with new pattern
+        //if old pattern is upper case, then the new pattern should also be upper case
+        //summary
+        static string ReplaceCaseInsensitive(string oldStr, string oldPattern, string newPattern)
+        {
+            MatchCollection collection = Regex.Matches(oldStr, oldPattern, RegexOptions.IgnoreCase);
+            string newStr = oldStr;
+            while(true){
+                Match c = Regex.Match(newStr, oldPattern, RegexOptions.IgnoreCase);
+                if (!c.Success) break;
+                if (!Regex.IsMatch(c.Value, "[A-Z]"))
+                {
+                    newStr = newStr.Remove(c.Index, oldPattern.Length);
+                    newStr = newStr.Insert(c.Index, newPattern);
+                }
+                else
+                {
+                    newStr = newStr.Remove(c.Index, oldPattern.Length);
+                    newStr = newStr.Insert(c.Index, newPattern.Replace(newPattern[0], Char.ToUpper(newPattern[0])));
+                }
+            }
+            return newStr;
+        }
+    }
+}
