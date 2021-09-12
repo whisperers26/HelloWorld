2dd93ad066b6830420342fff45575df1a4830b57
Liu Boning <liuboning2012@gmail.com>
2021-09-12 15:37:08 -0400

    complete PE8 q7

---
+++ David_PE8_Variables/PE8_q7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_q7
+{
+    //summary
+    //this program let user input a string and output reverse
+    //summary
+    class Program
+    {
+        //summary
+        //main method
+        //summary
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a string: ");
+            string input = Console.ReadLine();
+            string output = "";
+            for(int i = input.Length-1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
