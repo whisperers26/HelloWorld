5e3d7842404ad4ab9b7905bbd65ff96dd695b4ce
Liu Boning <liuboning2012@gmail.com>
2021-09-13 18:01:34 -0400

    complete PE9 q3

---
+++ David_PE9_Function/PE9_q3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_q3
+{
+    //summary
+    //this program impersonates Console.ReadLine()
+    //summary
+    class Program
+    {
+        //summary
+        //main method
+        //summary
+        static void Main(string[] args)
+        {
+            Del readLine = Console.ReadLine;
+            Console.WriteLine("Let's test the delegate function, please input a string: ");
+            string input = readLine();
+            Console.WriteLine("Your input is: {0}", input);
+        }
+
+        //delegate function
+        private delegate string Del();
+    }
+}
