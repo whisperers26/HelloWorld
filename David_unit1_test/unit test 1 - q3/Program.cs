ca2e7a7fa1d20243c215d095e27922182550a3b6
whisperers26 <liuboning98@163.com>
2021-09-21 17:53:12 -0400

    complete unit test 1 q3

---
+++ David_unit1_test/unit test 1 - q3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_test_1_q3
+{
+    //summary
+    //this program defines a delegate function of readline
+    //summary
+    class Program
+    {
+        //summary
+        //main method
+        //summary
+        static void Main(string[] args)
+        {
+            MyReadLine myReadLine;
+            myReadLine = new MyReadLine(Console.ReadLine);
+            Console.WriteLine("input sth:");
+            string input = myReadLine();
+            Console.WriteLine("You input: {0}", input);
+        }
+
+        delegate string MyReadLine();
+    }
+}
