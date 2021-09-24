1c5651cce79515e5bb2629e636f77f64d9c65fe6
whisperers26 <liuboning98@163.com>
2021-09-24 17:24:48 -0400

    complete pe11 q5,6

---
+++ David_PE11_Defining Classes/Defining Classes/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_Classes
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    public class Compact:Car, IPassengerCarrier
+    {
+
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+}
---
+++ David_PE11_Defining Classes/Traffic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Defining_Classes;
+
+namespace Traffic
+{
+    class Program
+    {
+        //summary
+        //main method
+        //summary
+        static void Main(string[] args)
+        {
+            SUV suv1 = new SUV();
+            AddPassenger(suv1);
+        }
+
+        //summary
+        //add passenger
+        //summary
+        public static void AddPassenger(IPassengerCarrier carrier)
+        {
+            carrier.LoadPassenger();
+            if (carrier.GetType().IsSubclassOf(typeof(Vehicle)))
+            {
+                Console.WriteLine(carrier.ToString());
+            }
+        }
+    }
+}
