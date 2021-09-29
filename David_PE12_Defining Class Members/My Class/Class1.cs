e223aaf5d968ef9ea07782c48c41015dae906e24
whisperers26 <liuboning98@163.com>
2021-09-28 22:07:36 -0400

    complete pe12 q3

---
+++ David_PE12_Defining Class Members/My Class/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Class
+{
+    public class MyClass
+    {
+        private string myString;
+
+        public string MyString
+        {
+            set => myString = value;
+        }
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    public class MyDerivedClass:MyClass
+    {
+        public override string GetString()
+        {
+            return (base.GetString() + " (output from the derived class)");
+        }
+    }
+}
---
+++ David_PE12_Defining Class Members/q3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using My_Class;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass obj1 = new MyDerivedClass();
+            obj1.MyString = "My string hahaha";
+            Console.WriteLine(obj1.GetString());
+        }
+    }
+}
