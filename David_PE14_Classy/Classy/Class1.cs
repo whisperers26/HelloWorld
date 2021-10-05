0b38dc183ff4e9bec63c2f4c636399a83a166249
whisperers26 <liuboning98@163.com>
2021-10-04 23:21:48 -0400

    complete PE14

---
+++ David_PE14_Classy/Classy/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classy
+{
+    public abstract class FinallyPE14lol
+    {
+        private string soManyHomeworks;
+
+        public string SoManyHomeworks
+        {
+            get => SoManyHomeworks;
+            set => soManyHomeworks = value;
+        }
+    }
+
+    public interface HowToDoHomworks
+    {
+        void DoHomeWork();
+    }
+
+    public class Way1ToDoHomeworks : HowToDoHomworks
+    {
+        public void DoHomeWork()
+        {
+            Console.WriteLine("Skip it!");
+        }
+    }
+
+    public class Way2ToDoHomeworks : HowToDoHomworks
+    {
+        public void DoHomeWork()
+        {
+            Console.WriteLine("Ignore it!");
+        }
+    }
+}
---
+++ David_PE14_Classy/MyApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classy;
+
+namespace MyApp
+{
+    //this program tests Classy
+    class Program
+    {
+        //main method
+        static void Main(string[] args)
+        {
+            Way1ToDoHomeworks way1 = new Way1ToDoHomeworks();
+            Way2ToDoHomeworks way2 = new Way2ToDoHomeworks();
+
+            Console.WriteLine("way 1 to do homeworks:");
+            MyMethod(way1);
+
+            Console.WriteLine("way 2 to do homeworks:");
+            MyMethod(way2);
+        }
+
+        public static void MyMethod(object myObject)
+        {
+            HowToDoHomworks letsDoHomeworks = (HowToDoHomworks)myObject;
+            letsDoHomeworks.DoHomeWork();
+        }
+    }
+
+    
+}
