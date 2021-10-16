b3a8d5c9ea40f12d398e50991be520fe24b225e4
whisperers26 <liuboning98@163.com>
2021-10-16 16:43:59 -0400

    complete q8-10

---
+++ David_unit2_test/Q10_App/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Q9_ClassLib;
+
+namespace Q10_App
+{
+    //app of q9
+    class Program
+    {
+        //main method
+        static void Main(string[] args)
+        {
+            SoftPenCalligraphy softWrite = new SoftPenCalligraphy();
+            HardPenCalligraphy hardWrite = new HardPenCalligraphy();
+            myMethod(softWrite);
+            myMethod(hardWrite);
+        }
+
+        static void myMethod(object obj)
+        {
+            IGrind grind;
+            ISharpen sharpen;
+            Calligraphy write;
+            SoftPenCalligraphy softWrite;
+            HardPenCalligraphy hardWrite;
+            Type type = obj.GetType();
+            if (type.IsSubclassOf(typeof(Calligraphy)))
+            {
+                write = (Calligraphy)obj;
+                write.Content = "test";
+            }
+            if (type.Equals(typeof(SoftPenCalligraphy)))
+            {
+                grind = (IGrind)obj;
+                grind.PrepareInk();
+                softWrite = (SoftPenCalligraphy)obj;
+                softWrite.Write();
+            }
+            if (type.Equals(typeof(HardPenCalligraphy)))
+            {
+                sharpen = (ISharpen)obj;
+                sharpen.SharpenPencil();
+                hardWrite = (HardPenCalligraphy)obj;
+                hardWrite.Write();
+            }
+        }
+    }
+}
---
+++ David_unit2_test/Q9_ClassLib/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9_ClassLib
+{
+    public abstract class Calligraphy
+    {
+        private string content;
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+        public abstract void Write();
+    }
+
+    public interface IGrind
+    {
+        void PrepareInk();
+    }
+
+    public interface ISharpen
+    {
+        void SharpenPencil();
+    }
+
+    public class SoftPenCalligraphy:Calligraphy, IGrind
+    {
+        public void PrepareInk()
+        {
+
+        }
+
+        public override void Write()
+        {
+            
+        }
+    }
+
+    public class HardPenCalligraphy : Calligraphy, ISharpen
+    {
+        public void SharpenPencil()
+        {
+
+        }
+
+        public override void Write()
+        {
+
+        }
+    }
+}
