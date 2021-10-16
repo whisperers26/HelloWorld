761b82e02ce1f9832e27f06a3c6cca4175dabbd9
whisperers26 <liuboning98@163.com>
2021-10-16 16:03:22 -0400

    complete q4567

---
+++ David_unit2_test/Q4_ClassLib/Class1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_ClassLib
+{
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set => phoneNumber = value;
+        }
+
+        public abstract void Connect();
+
+        public abstract void Disconnect();
+    }
+
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+            
+        }
+
+        public override void Disconnect()
+        {
+            
+        }
+    }
+
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get => whichDrWho;
+        }
+
+        private string FemaleSideKick
+        {
+            get => femaleSideKick;
+        }
+
+        public void TimeTravel()
+        {
+
+        }
+
+        public static bool operator ==(Tardis a, Tardis b) => a.whichDrWho == b.whichDrWho;
+
+        public static bool operator !=(Tardis a, Tardis b) => a.whichDrWho != b.whichDrWho;
+
+        public static bool operator <(Tardis a, Tardis b)
+        {
+            if (a.whichDrWho == 10) return false;
+            else if (b.whichDrWho == 10 && a.whichDrWho != 10) return true;
+            else return a.whichDrWho < b.whichDrWho;
+        }
+
+        public static bool operator >(Tardis a, Tardis b) => !(a < b) && (a != b);
+
+        public static bool operator >=(Tardis a, Tardis b) => !(a < b);
+
+        public static bool operator <=(Tardis a, Tardis b) => !(a > b);
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+}
---
+++ David_unit2_test/Q6_Program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Q4_ClassLib;
+
+namespace Q6_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tardis tardis = new Tardis();
+            PhoneBooth phonebooth = new PhoneBooth();
+        }
+
+        static void UsePhone(object obj)
+        {
+            try
+            {
+                PhoneInterface phoneInterface = (PhoneInterface)obj;
+                phoneInterface.MakeCall();
+                phoneInterface.HangUp();
+                if (obj.GetType().Equals(typeof(PhoneBooth)))
+                {
+                    PhoneBooth phonebooth = (PhoneBooth)obj;
+                    phonebooth.OpenDoor();
+                }
+                else if (obj.GetType().Equals(typeof(Tardis)))
+                {
+                    Tardis tardis = (Tardis)obj;
+                    tardis.TimeTravel();
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid obj");
+            }
+            
+        }
+    }
+}
