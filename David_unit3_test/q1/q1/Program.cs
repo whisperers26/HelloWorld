5c7f716f7c2ea795a30395b2aec0da898052abba
David Liu <liuboning2012@gmail.com>
2021-11-17 00:20:22 -0500

    complete q1-3

---
+++ David_unit3_test/q1/q1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace q1
+{
+    //main class
+    class Program
+    {
+        //execute method
+        static void Main(string[] args)
+        {
+            //prompt user for a string
+            Console.WriteLine("Please input a string: ");
+            string userInput = Console.ReadLine();
+
+            //print the value of each letter
+            char[] userInputValues = CalculateStrVal(userInput);
+            for(int i = 0; i < userInput.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", userInput[i], (int)userInputValues[i]);
+            }
+
+            //print the string in reverse order
+            Console.WriteLine("\nReverse string is {0}", ReverseStr(userInput));
+
+            //print if the string is palindrome
+            //Console.WriteLine(RemovePuncts(userInput));
+            Console.WriteLine("\nThe string is a palindrome? {0}", IsPalindrome(userInput));
+        }
+
+        //calculate how many of each letter of the alphabet are in the string (case-insensitive, ie. A=a)
+        static char[] CalculateStrVal(string str)
+        {
+            ArrayList list = new ArrayList();
+            char[] charArr = str.ToCharArray();
+            for(int i = 0; i < charArr.Length; i++)
+            {
+                if(charArr[i]>='A'&& charArr[i] <= 'Z')
+                {
+                    charArr[i] = (char)(charArr[i] + 'a' - 'A');
+                }
+            }
+            return charArr;
+        }
+
+        //convert a string to reverse order
+        static string ReverseStr(string str)
+        {
+            char[] reverseChars = str.ToCharArray();
+            for(int i = 0; i < str.Length; i++)
+            {
+                reverseChars[i] = str[str.Length - i - 1];
+            }
+            string reverseStr = new string(reverseChars);
+            return reverseStr;
+        }
+
+        //remove all the chars except a-z, A-Z in a string
+        static string RemovePuncts(string str)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(!((str[i]>='a'&& str[i] <= 'z')||(str[i] >= 'A'&& str[i] <= 'Z')))
+                {
+                    str = str.Remove(i, 1);
+                    i--;
+                }
+            }
+            return str;
+        }
+
+        // judge if the string is a palindrome (allowing for punctuation, spaces and different capitalization)
+        // For example,  "Madam, I'm Adam" is a palindrome.
+        static bool IsPalindrome(string str)
+        {
+            str = RemovePuncts(str);
+            str = new string(CalculateStrVal(str));
+            if (str == ReverseStr(str)) return true;
+            else return false;
+        }
+    }
+}
---
+++ David_unit3_test/q2-5_DirectedGraph/q2-5_DirectedGraph/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2_5_DirectedGraph
+{
+    class Program
+    {
+        //Red:0, Blue:1, Grey:2, Light Blue:3, Yellow:4, Pruple:5, Orange:6, Green:7
+
+        //adjacent matrix of the graph
+        private static int[,] mGraph = new int[,]
+        {
+            // 0 1 2 3 4 5 6 7
+            /*0*/{-1,1,5,-1,-1,-1,-1,-1, },
+            /*1*/{-1,-1,-1,1,8,-1,-1,-1, },
+            /*2*/{-1,-1,-1,0,-1,-1,1,-1 },
+            /*3*/{-1,1,0,-1,-1,-1,-1,-1 },
+            /*4*/{-1,-1,-1,-1,-1,-1,-1,6 },
+            /*5*/{-1,-1,-1,-1,1,-1,-1,-1 },
+            /*6*/{-1,-1,-1,-1,-1,1,-1,-1 },
+            /*7*/{-1,-1,-1,-1,-1,-1,-1,-1 }
+        };
+
+        //adjacent list of the graph
+        private static (int vertex, int edge)[][] lGraph = new (int, int)[][]
+        {
+            /*0*/new (int,int)[]{(1,1),(2,5) },
+            /*1*/new (int,int)[]{(3,1) },
+            /*2*/new (int,int)[]{(3,0),(6,1) },
+            /*3*/new (int,int)[]{(1,1),(2,0) },
+            /*4*/new (int,int)[]{(7,6) },
+            /*5*/new (int,int)[]{(4,1) },
+            /*6*/new (int,int)[]{(5,1) },
+            /*7*/new (int,int)[]{ },
+        };
+
+
+        //main method
+        static void Main(string[] args)
+        {
+            DFS(0);
+        }
+
+        //DFS 
+        public static void DFS(int startV)
+        {
+            bool[] visited = new bool[lGraph.Length];
+            DFShelper(startV, visited);
+        }
+
+        //DFS helper function
+        private static void DFShelper(int v, bool[] visited)
+        {
+            //set vertex to visited and record
+            visited[v] = true;
+            Console.Write("{0} ", v);
+
+            //recur all
+            (int vertex, int edge)[] vList = lGraph[v];
+            foreach (var n in vList)
+            {
+                if (!visited[n.vertex])
+                    DFShelper(n.vertex, visited);
+            }
+        }
+
+    }
+}
