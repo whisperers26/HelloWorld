90dcf6de69ef1381393fea3eb546c7ece55cfc4d
David Liu <liuboning2012@gmail.com>
2021-11-17 17:41:35 -0500

    complete q2-5

--- David_unit3_test/q2-5_DirectedGraph/q2-5_DirectedGraph/Program.cs
+++ David_unit3_test/q2-5_DirectedGraph/q2-5_DirectedGraph/Program.cs
@@ -9,7 +9,7 @@ namespace q2_5_DirectedGraph
     class Program
     {
         //Red:0, Blue:1, Grey:2, Light Blue:3, Yellow:4, Pruple:5, Orange:6, Green:7
-
+        
         //adjacent matrix of the graph
         private static int[,] mGraph = new int[,]
         {
@@ -41,7 +41,27 @@ namespace q2_5_DirectedGraph
         //main method
         static void Main(string[] args)
         {
+            Console.WriteLine("DFS of vertex 0: ");
             DFS(0);
+
+            //use the  linked list version
+            Graph graph = new Graph(8);
+            graph.AddEdge(0, 1, 1);
+            graph.AddEdge(0, 2, 5);
+            graph.AddEdge(1, 3, 1);
+            graph.AddEdge(2, 3, 0);
+            graph.AddEdge(2, 6, 1);
+            graph.AddEdge(3, 1, 1);
+            graph.AddEdge(3, 2, 0);
+            graph.AddEdge(4, 7, 6);
+            graph.AddEdge(5, 4, 1);
+            graph.AddEdge(6, 5, 1);
+
+            Console.WriteLine("Graph:");
+            graph.PrintGraph();
+            //Console.WriteLine("distance from 4 to 7: {0}", graph.CalculateCost(4,7));
+            int[] distances = graph.dijkstra(0);
+            for (int i = 0; i < distances.Length; i++) Console.WriteLine("Distance from 0 to {0}: {1}", i, distances[i]);
         }
 
         //DFS 
@@ -49,6 +69,7 @@ namespace q2_5_DirectedGraph
         {
             bool[] visited = new bool[lGraph.Length];
             DFShelper(startV, visited);
+            Console.WriteLine();
         }
 
         //DFS helper function
@@ -65,6 +86,142 @@ namespace q2_5_DirectedGraph
                 if (!visited[n.vertex])
                     DFShelper(n.vertex, visited);
             }
+        }
+
+    }
+
+    //true adjacent list implementation of graph
+    //not only representation
+    //include shortest path algorithm
+    class Graph
+    {
+        List<LinkedList<(int destination, int edge)>> graph;
+
+        //constructor
+        public Graph(int vertices)
+        {
+            graph = new List<LinkedList<(int, int)>>();
+            for(int i = 0; i < vertices; i++)
+            {
+                graph.Add(new LinkedList<(int destination, int edge)>());
+            }
+        }
+
+        //find a node
+        public int FindNode(int vertex)
+        {
+            if (vertex >= 0 && vertex < graph.Capacity)
+            {
+                return vertex;
+            }
+            else return -1;
+        }
+        
+        //add a node
+        public void AddNode()
+        {
+            graph.Add(new LinkedList<(int destination, int edge)>());
+        }
+
+        //add a edge
+        public bool AddEdge(int origin, int destination, int edge)
+        {
+            if (this.FindNode(origin) >= 0 && this.FindNode(destination) >= 0)
+            {
+                graph[origin].AddLast((destination, edge));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //remove an edge
+        public bool RemoveEdge(int origin, int destination, int edge)
+        {
+            (int, int) item = (destination, edge);
+            return graph[origin].Remove(item);
+        }
+
+        //calculate cost from a to b if an edge exists
+        public int CalculateCost(int origin, int destination)
+        {
+            foreach(var item in graph[origin])
+            {
+                if (item.destination == destination) return item.edge;
+            }
+            return -1;
+        }
+
+        //print graph
+        public void PrintGraph()
+        {
+            foreach(var linkedList in graph)
+            {
+                Console.Write("{0}: ", graph.IndexOf(linkedList));
+                foreach((int destination, int edge) item in linkedList)
+                {
+                    Console.Write("({0}, {1})", item.destination, item.edge);
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+        //dijsktra's algorithm to find the shortest path from origin to all other points
+        //calculate mini distance and return the vertex
+        private int MiniDistance(int[] dist, bool[] sptSet)
+        {
+            int minCost = int.MaxValue;
+            int minVertex = -1;
+            for(int i = 0; i < graph.Capacity; i++)
+            {
+                if (sptSet[i] == false && dist[i] <= minCost)
+                {
+                    minCost = dist[i];
+                    minVertex = i;
+                }
+            }
+            return minVertex;
+        }
+
+        //dijkstra
+        public int[] dijkstra(int origin)
+        {
+            //output array
+            int[] dist = new int[graph.Capacity];
+            //sptset
+            bool[] sptSet = new bool[graph.Capacity];
+
+            for(int i = 0; i < graph.Capacity; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            //initialize distance of origin
+            dist[origin] = 0;
+
+            //find shortest paths
+            for(int vertex = 0; vertex < graph.Capacity - 1; vertex++)
+            {
+                //from vertices outside the sptset, choose one with minidistance
+                int v1 = MiniDistance(dist, sptSet);
+                sptSet[v1] = true;
+
+
+                //move on to the adjacent vertices and calculate minidistance
+                for(int v2 = 0; v2 < graph.Capacity; v2++)
+                {
+                    if (!sptSet[v2] && CalculateCost(v1, v2) >= 0 && dist[v1] != int.MaxValue && dist[v1] + CalculateCost(v1, v2) < dist[v2])
+                    {
+                        dist[v2] = dist[v1] + CalculateCost(v1, v2);
+                    }
+                }
+
+            }
+            return dist;
         }
 
     }
